a4ff153fc01a2e1635b0e77d8ecf640b1f777aac
ivan12983 <ivan6386caps@gmail.com>
2026-09-14 11:44:02 -0300

    Refactor server to use environment variable for PORT

--- backend/server.js
+++ backend/server.js
@@ -187,6 +187,8 @@ app.get("/medicacoes", (req, res) => {
 });
 
 // START
-app.listen(3000, () => {
-  console.log("🏥 Hospital Pro rodando em http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`🏥 Hospital Pro rodando na porta ${PORT}`);
 });
